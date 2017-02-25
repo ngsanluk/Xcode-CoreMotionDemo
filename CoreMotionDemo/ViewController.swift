6199a66f9ca4f23e42c6b968246698a40eb19a4e
Sunny NG <ngsanluk@gmail.com>
2017-02-25 15:54:11 +0800

    First commit

--- CoreMotionDemo/ViewController.swift
+++ CoreMotionDemo/ViewController.swift
@@ -7,17 +7,54 @@
 //
 
 import UIKit
+import CoreMotion
+
 
 class ViewController: UIViewController {
 
+    var motionManager: CMMotionManager!
+    
+    @IBOutlet weak var xLabel: UILabel!
+    @IBOutlet weak var yLabel: UILabel!
+    @IBOutlet weak var zLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.motionManager = CMMotionManager()
+        self.motionManager.accelerometerUpdateInterval = 0.1
+        
+        if motionManager.isAccelerometerAvailable {
+            print("acceleromet is available")
+            motionManager.startAccelerometerUpdates(to: OperationQueue.main,
+                withHandler: { (accelerometerData, error) -> Void in
+                    if error != nil {
+                        self.motionManager.stopAccelerometerUpdates()
+                    } else {
+                        self.xLabel.text = String(format: "%f", (accelerometerData?.acceleration.x)!)
+                        self.yLabel.text = String(format: "%f", (accelerometerData?.acceleration.y)!)
+                        self.zLabel.text = String(format: "%f", (accelerometerData?.acceleration.z)!)
+                    }
+                }
+            )
+            
+        }
+        else {
+            print("acceleromet is not available")
+
+        }
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        self.motionManager.stopAccelerometerUpdates()
     }
 
 
